8d8f5277d36d6fb3372404c6c9f518c13c9ae8cb
Peter Regard <faceofboe.who@gmail.com>
2015-10-08 18:46:59 -0400

    Fixed moving maze, added Simpler maze, now interacts with maze in right way

--- Maze/GameScene.swift
+++ Maze/GameScene.swift
@@ -8,14 +8,38 @@
 
 import SpriteKit
 var player = SKSpriteNode()
-
-class GameScene: SKScene {
+var player2 = SKSpriteNode()
+var maze = SKSpriteNode()
+var mazeTexture = SKTexture(imageNamed: "Simple Maze")
+class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMoveToView(view: SKView) {
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         /* Setup your scene here */
-       
+        
+//        player2 = childNodeWithName("Player2") as! SKSpriteNode
+//        player2.zPosition = 2
+//        player2.physicsBody?.contactTestBitMask = 3
+//        
+
         player = childNodeWithName("Player") as! SKSpriteNode
-    
-    
+        player.zPosition = 2
+        player.physicsBody?.contactTestBitMask = 3
+        player.physicsBody?.collisionBitMask = 3
+        player.physicsBody?.categoryBitMask = 3
+
+        
+        maze = childNodeWithName("Maze") as! SKSpriteNode
+        maze.physicsBody?.dynamic = false
+        maze.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        maze.physicsBody?.contactTestBitMask = 3
+        maze.physicsBody?.collisionBitMask = 1
+        maze.physicsBody?.categoryBitMask = 1
+//        maze.physicsBody = SKPhysicsBody(texture: mazeTexture, alphaThreshold: 0.01, size: CGSizeMake(1025.285, 764.958))
+        
+        
+        
+        maze.zPosition = 2
     }
     
     override func mouseDown(theEvent: NSEvent) {
@@ -71,10 +95,15 @@ class GameScene: SKScene {
             player.physicsBody?.velocity = CGVectorMake(0, 0)
             }
             }
-
-
+}
+    func didBeginContact(contact: SKPhysicsContact) {
+    
     }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        
+        
     }
 }
